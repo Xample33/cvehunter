cbdbfa08364f502c966d791c0f60807561905f2e
Peter Minerba <54323615+Xample33@users.noreply.github.com>
2023-10-05 00:11:01 +0200

    improved session close

--- cvehunter/Auth.py
+++ cvehunter/Auth.py
@@ -1,30 +1,32 @@
-from .Cve import Cve
-from .Cvss import Cvss
-from .Cwe import Cwe
-
-from urllib3.exceptions import InsecureRequestWarning
-from urllib3 import disable_warnings
-import json
-from cvehunter import utils as u
 import httpx
-from httpx import HTTPStatusError, ConnectTimeout
-from datetime import datetime, timezone
-#disable warnings
-disable_warnings(InsecureRequestWarning)
+from httpx import ConnectTimeout, HTTPStatusError
 
 class Auth:
     def __init__(self, username: str, password: str, proxy: dict = None):
-        
         self.username = username
         self.password = password
         self.proxy = proxy
-        
         self.timeout = 10
-        self.client = httpx.AsyncClient(auth=(self.username, self.password), verify=False, timeout=self.timeout, proxies=self.proxy)
+        self.client = None  # Initialize the client as None
+
+    async def get_client(self):
+        if self.client is None or self.client.is_closed:
+            # Create a new client or recreate it if it's closed
+            self.client = httpx.AsyncClient(
+                auth=(self.username, self.password),
+                verify=False,
+                timeout=self.timeout,
+                proxies=self.proxy
+            )
+        return self.client
 
     async def check_connection(self):
         api_url = "https://www.opencve.io/api/account/subscriptions/vendors"
-        response = await self.client.get(api_url)
+        client = await self.get_client()
+        try:
+            response = await client.get(api_url)
+        except ConnectTimeout:
+            raise ConnectTimeout("Connection timed out")
 
         if response.status_code == 200:
             return
@@ -33,19 +35,22 @@ class Auth:
         else:
             raise HTTPStatusError(f"Error {response.status_code} while retrieving data from {api_url}")
 
-    async def make_request(self, api_url: str):
+    async def make_request(self, api_url: str) -> str:
+        client = await self.get_client()
         try:
-            response = await self.client.get(api_url)
+            response = await client.get(api_url)
         except ConnectTimeout:
-            raise ConnectTimeout(f"Connection timed out")	
-        
+            raise ConnectTimeout("Connection timed out")
+
         if response.status_code == 401:
             raise ValueError("Invalid credentials")
-        
+
         if '{"message": "Not found."}' in response.text:
             return None
         
+        await self.close_connection()
         return response.text
-    
+
     async def close_connection(self):
-        await self.client.aclose()
+        if self.client and not self.client.is_closed:
+            await self.client.aclose()
