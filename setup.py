3d36836949bb9d466fbbea0694b4a2e357c02183
Xample <54323615+Xample33@users.noreply.github.com>
2023-10-06 16:52:53 +0200

    updated version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ def read(fname):
 
 setup(
     name="cvehunter",
-    version="1.0.1",
+    version="1.0.2",
     author="Xample33",
     license="MIT",
     url="https://github.com/Xample33/cvehunter",
