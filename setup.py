2d9b5c1fe177e626804b365a6d13f067b98b0355
Peter Minerba <54323615+Xample33@users.noreply.github.com>
2023-10-06 16:11:10 +0200

    fixed long_description and added type

--- setup.py
+++ setup.py
@@ -2,15 +2,16 @@ from os import path
 from setuptools import setup
 
 def read(fname):
-    return open(path.join(path.dirname(__file__), fname)).read()
+    return open(path.join(path.dirname(__file__), fname), encoding='utf-8').read()
 
 setup(
-    name = "cvehunter",
-    version = "0.0.3",
-    author = "Xample33",
-    license = "MIT",
-    url = "https://github.com/Xample33/cvehunter",
+    name="cvehunter",
+    version="0.0.3",
+    author="Xample33",
+    license="MIT",
+    url="https://github.com/Xample33/cvehunter",
     long_description=read('README.md'),
+    long_description_content_type='text/markdown',  # Specify the content type as Markdown
     install_requires=[
         "httpx",
         "urllib3",
