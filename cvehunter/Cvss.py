784aff963f4272db4317224be615fa883c733591
Peter Minerba <54323615+Xample33@users.noreply.github.com>
2023-10-09 12:24:16 +0200

    added __eq__

--- cvehunter/Cvss.py
+++ cvehunter/Cvss.py
@@ -6,6 +6,13 @@ class Cvss:
         self.version: float = None
         self.exploitability: float = None
         self.impact: float = None
+        
+    def __eq__(self, other):
+        if isinstance(other, dict):
+            return vars(self) == other
+        elif isinstance(other, Cvss):
+            return vars(self) == vars(other)
+        return False
 
     def __repr__(self) -> str:
         # Return a string representation that can recreate the object
